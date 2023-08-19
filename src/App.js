cd5c0a2c4c46a8ca40091df632b92eb967c59ef7
SreenijaPuvvula <psreenija2359@gmail.com>
2023-08-19 14:16:46 +0530

    Project 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,129 @@
-import logo from './logo.svg';
-import './App.css';
-
+import axios from "axios";
+import "./styles.css";
+import { useState } from "react";
 function App() {
+
+  const [data, setData] = useState([]);
+  const [newData, setNewData] = useState({
+    title: "",
+    body: ""
+  })
+
+  function getData() {
+    axios({
+      url: "https://jsonplaceholder.typicode.com/posts",
+      method: "GET",
+    })
+      .then((response) => {
+        console.log(response.data);
+      })
+  }
+
+  function removeData() {
+    setData([])
+  }
+
+  function createCard(cardData) {
+    return (
+      <div key={cardData.id}>
+        <h3>{cardData.title}</h3>
+        <p>{cardData.content}</p>
+      </div>
+    )
+  }
+
+  function getProducts() {
+    axios({
+      url: "http://localhost:8000/products",
+      method: "GET",
+    }).then((res) => {
+      setData(res.data)
+    })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  function insertProducts() {
+    axios({
+      url: "http://localhost:8000/products",
+      method: "POST",
+      data: newData
+    }).then(() => {
+      console.log("Data submitted");
+    })
+      .catch(() => {
+        console.log("Error in submition");
+      })
+  }
+
+  function handleChange(event) {
+    setNewData((prevState) => ({
+      ...prevState,
+      [event.target.name]: event.target.value
+    }))
+  }
+
+  function handleDelete(){
+    axios({
+      url:"http://localhost:8000/products",
+      method:"DELETE",
+      data:newData
+    })
+    .then((res) => {
+      console.log("Data Deleted")
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+  }
+
+  function handleUpdate(){
+    axios({
+      url:"http://localhost:8000/products",
+      method:"PUT",
+      data:newData
+    }).then((res) => {
+      console.log("Data Updated")
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+  }
+
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>REST Api</h1>
+
+      <button onClick={getData}>GET</button>
+      <button onClick={removeData}>CLOSE</button>
+      <h2>Products</h2>
+      <div>
+        {data.map(createCard)}
+      </div>
+      <button onClick={getProducts}>Get Products</button>
+      <h2>Insert Products</h2>
+      <form >
+        <input type="text" placeholder="title" name="title" onChange={handleChange}></input>
+        <input type="text" placeholder="value" name="body" onChange={handleChange}></input>
+        <button onClick={insertProducts} >Insert Product</button>
+      </form>
+      <div>
+        <input type="text" placeholder="enter title to delete" onChange={handleChange} name="title"></input>
+        <button onClick={handleDelete}>Delete</button>
+      </div>
+      <div>
+        <input type="text" placeholder="enter title to update" onChange={handleChange} name="title"></input>
+        <input type="text" placeholder="value" name="body" onChange={handleChange}></input>
+        <button onClick={handleUpdate}>Update</button>
+      </div>
+      <div>
+
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
